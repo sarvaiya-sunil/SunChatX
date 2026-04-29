876dbb4f3f79f6ad9514ef8b15d154b8b2fc2a42
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-04-29 16:43:34 +0530

    signup controller added

---
+++ backend/src/controllers/auth-controller.js
@@ -0,0 +1,50 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "all fields are required" });
+    }
+    if (password.length < 4) {
+      return res
+        .status(400)
+        .json({ message: "password must be at least 4 characters" });
+    }
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "enter vaid email address" });
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exist" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "invalid user data" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error " });
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,26 +3,33 @@ import dotenv from "dotenv";
 import path from "path";
 import authRoutes from "./routes/auth-route.js";
 import messageRoutes from "./routes/message-route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
-const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+//const __dirname = path.resolve();
+
 // make ready fro deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 // npm run build -> for installing node_modules in frontend and backend
 // npm run start -> for starting backend script
