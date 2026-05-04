65f2287dd011a99992568e3696da8dd2634c2184
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-05-04 12:28:52 +0530

    signup-UI implemented

--- frontend/src/stores/useAuthStore.js
+++ frontend/src/stores/useAuthStore.js
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: false,
+  isSigningUp: false,
 
   checkAuth: async () => {
     set({ isCheckingAuth: true });
@@ -16,6 +18,21 @@ export const useAuthStore = create((set) => ({
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signUp: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+
+      toast.success("Account created successfully!");
+    } catch (error) {
+      set({ authUser: null });
+      toast.error(error.response?.data?.message || "Failed to create account");
+    } finally {
+      set({ isSigningUp: false });
     }
   },
 }));
