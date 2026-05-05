f8bb52d58cde3f140cf63c3b9eabb71fe635af6d
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-05-05 15:00:26 +0530

    chat container setup done

---
+++ frontend/src/components/ChatHeader.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { useChatStore } from "../stores/useChatStore";
+import { XIcon } from "lucide-react";
+
+function ChatHeader() {
+  const { selectedUser, setSelectedUser } = useChatStore();
+
+  useEffect(() => {
+    const handleEscKey = (event) => {
+      if (event.key === "Escape") setSelectedUser(null);
+    };
+    window.addEventListener("keydown", handleEscKey);
+    //Cleanup function
+    return () => window.removeEventListener("keydown", handleEscKey);
+  }, [selectedUser]);
+
+  return (
+    <div className="flex items-center justify-between bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1">
+      <div className="flex items-center space-x-3">
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img
+              src={selectedUser.profilePic || "/avatar.png"}
+              alt={selectedUser.fullName}
+            />
+          </div>
+        </div>
+        <div>
+          <h3 className="text-slate-200 font-medium">
+            {selectedUser.fullName}
+          </h3>
+          <p className="text-slate-400 text-xs">Online</p>
+        </div>
+      </div>
+      <button onClick={() => setSelectedUser(null)}>
+        <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
+      </button>
+    </div>
+  );
+}
+
+export default ChatHeader;
