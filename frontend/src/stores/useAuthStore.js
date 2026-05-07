3813a49932441b8ab36ad81618f8a0aba30dc48d
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-05-07 12:49:44 +0530

    changed package.json file

--- frontend/src/stores/useAuthStore.js
+++ frontend/src/stores/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { Navigate } from "react-router";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
