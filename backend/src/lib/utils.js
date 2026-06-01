f765e4f993ca8a7aa51f6d1bd1939534b51457d9
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-06-01 16:10:33 +0530

    deployment changes

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, //MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", //CSFR attacks
-    secure: false,
+    sameSite: "none", //CSFR attacks
+    secure: true,
   });
 
   return token;
